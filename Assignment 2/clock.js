4a55652955d86a0e129adb978543a90c86c075ea
kundrat1280 <kundrat1280@yahoo.com>
2016-09-24 10:53:34 -0400

    Assignment 2
    Unobtrusive javascript

---
+++ Assignment 2/clock.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  displayTime();
+}
+function displayTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m + ":" + s;
+	setTimeout(displayTime, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
