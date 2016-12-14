8d0fca098f8194c9ee61c475ae4bf8fa9e2c873f
kundrat1280 <kundrat1280@yahoo.com>
2016-12-13 20:38:38 -0500

    Updated with logon script

--- Assignment 8/FinalProject/Scripts/Home.js
+++ Assignment 8/FinalProject/Scripts/Home.js
@@ -1,1 +1,21 @@
-﻿
+﻿$(document).ready(function () {
+
+    var username = $(".username");
+    var password = $(".password")
+    var logbutton = $(".logbutton");
+    var output = $(".output");
+
+    logbutton.click(function () {
+        $.ajax({
+            url: "Home/Login",
+            data: {
+                "Username": username,
+                "Password": password
+            },
+            success: function (stringResponse) {
+                response = JSON.parse(stringResponse);
+                response.Message == "Success" ? Home.GetAccount(username, usernameMsg) : Home.LoginError(username, usernameMsg, data);
+            }
+        });
+    });
+});
