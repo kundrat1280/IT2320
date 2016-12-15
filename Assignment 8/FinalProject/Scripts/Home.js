8b0739123202ced3a3111923df646de91a520dac
kundrat1280 <kundrat1280@yahoo.com>
2016-12-14 22:38:37 -0500

    update css

--- Assignment 8/FinalProject/Scripts/Home.js
+++ Assignment 8/FinalProject/Scripts/Home.js
@@ -1,25 +1,76 @@
 ﻿$(document).ready(function () {
 
-    var username = $(".username").val();
-    var password = $(".password").val();
+    var username = $(".username");
+    var password = $(".password");
     var logbutton = $(".logbutton");
     var output = $(".output");
+    var crbutton = $(".crbutton");
+    var crusername = $(".crusername");
+    var crpassword = $(".crpassword");
+    var email = $(".email");
+    var repemail = $(".repemail");
+    var elemname = $(".elemname");
+    var elempassword = $(".elempassword");
+    var infousername = $(".infousername");
+    var infoemail = $(".infoemail");
+    var infogpa = $(".infogpa");
 
     logbutton.click(function () {
         $.ajax({
-            type: "POST",
             url: "Home/Login",
-            dataType: 'json',
-            async: false,
             data: {
-                "Username": username,
-                "Password": password
+                "Username": username.val(),
+                "Password": password.val()
             },
             success: function (stringResponse) {
                 response = JSON.parse(stringResponse);
-                response.Message == "Success" ? Home.GetAccount(username, usernameMsg) : Home.LoginError(username, usernameMsg, data);
-                output.html(response.usernameMsg);
+                $(".content").css("display", "none");
+                $(".output").css("display", "inline");
+                
+            }
+        });
+        $.ajax({
+            url: "Home/GetAccountInformation",
+            data: {
+                "Username": username.val()
+            },
+            success: function (stringResponse) {
+                response = JSON.parse(stringResponse);
+                payload = response.Payload;
+                user = JSON.parse(payload);
+                infousername.val(user.account.username);
+                infoemail.val(user.account.emailadd);
+            }
+        })
+    });
+
+    crbutton.click(function () {
+        $.ajax({
+            url: "Home/CreateAccount",
+            data: {
+                "Username": crusername.val(),
+                "Password": crpassword.val(),
+                "EmailAdd": email.val(),
+                "EmailCon": repemail.val()
+            },
+            success: function (stringResponse) {
+                response = JSON.parse(stringResponse);
             }
         });
     });
+
+    $(".upemlbutton").click(function () {
+        $.ajax({
+            url: "Home/AddOrUpdateElement",
+            data: {
+                "Username": infousername.val(),
+                "ElementName": "emailadd",
+                "ElementValue": infoemail.val()
+            },
+            success: function (stringResponse) {
+                response = JSON.parse(stringResponse);
+                alert(response.Message);
+            }
+        })
+    })
 });
